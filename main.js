74ff847e336ea58e2160543db1efdcd95bbb6d99
Tadaomi Kasuya <dtl.reco@gmail.com>
2023-06-22 00:32:08 +0700

    test

--- main.js
+++ main.js
@@ -14,15 +14,23 @@
 
 const siteKey = document.getElementById('recap').getAttribute('data-sitekey'); // reCAPTCHAのsitekeyを取得
 
-document.querySelector('.bl_form').addEventListener('submit', function(e) {
+// フォーム送信時の処理を定義
+function handleFormSubmit(e) {
   e.preventDefault(); // submitを止める
+
   grecaptcha.ready(function() {
     grecaptcha.execute(siteKey, { action: 'submit' }).then(function(token) {
       // reCAPTCHAを実行
       document.getElementById('recap').value = token; // 戻り値tokenを#recapに格納
-      document.querySelector('.bl_form').removeEventListener('submit', arguments.callee); // イベントリスナーの削除
+      document.querySelector('.bl_form').removeEventListener('submit', handleFormSubmit); // イベントリスナーの削除
       document.querySelector('.bl_form').submit(); // formをsubmit
       document.querySelector('.bl_form_message').style.display = 'block'; // 送信後メッセージを表示
+    }).catch(function(error) {
+      console.error('reCAPTCHA execution failed:', error);
+      // エラーハンドリングを行う場合はここに処理を記述する
     });
   });
-});
+}
+
+// フォームのsubmitイベントにイベントリスナーを登録
+document.querySelector('.bl_form').addEventListener('submit', handleFormSubmit);
