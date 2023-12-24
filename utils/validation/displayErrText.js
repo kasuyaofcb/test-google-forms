ea54d738e3cc0d6f598979c828ee2f5ae5547ca6
Tadaomi Kasuya <dtl.reco@gmail.com>
2023-12-24 10:51:07 +0700

    fix:if文完結化

--- utils/validation/displayErrText.js
+++ utils/validation/displayErrText.js
@@ -1,6 +1,3 @@
-
-
-
 export const displayErrText = (event, type, val, formEle, msgEle, regex) => {
 	const setErrText = (errText) => {
 		formEle.classList.add("pageForm__item--valiOn");
@@ -20,40 +17,27 @@ export const displayErrText = (event, type, val, formEle, msgEle, regex) => {
 	);
 	const nameTextDoc = nameElementDoc ? nameElementDoc.value : "";
 
-
 	const kanjiKanaRegex = /^[ぁ-んァ-ヶー一-龯]+$/;
 
-	if (type !== "post") {
-		if (val.length === 0) {
-			setErrText("正しく入力してください。");
-			return;
-		}
-	}
-	if (type === "tell") {
-		if (!libphonenumber.isValidNumber(val, "JP") || !val.match(regex)) {
-			setErrText("正しく入力してください。");
-		} else {
-			formEle.classList.remove("pageForm__item--valiOn");
-		}
-	} else if (type === "post") {
-		if (!val.match(regex) && val.length !== 0) {
-			setErrText("正しく入力してください。");
-		} else {
-			formEle.classList.remove("pageForm__item--valiOn");
-		}
+	if (
+		type === "tell" &&
+		(!libphonenumber.isValidNumber(val, "JP") || !val.match(regex))
+	) {
+		setErrText("正しく入力してください。");
+	} else if (type === "post" && !val.match(regex) && val.length !== 0) {
+		setErrText("正しく入力してください。");
+	} else if (!val.match(regex)) {
+		const isPreventEn = preventEn || preventEnDoc;
+		const isValidName =
+			kanjiKanaRegex.test(nameText) && kanjiKanaRegex.test(nameTextDoc);
+
+		setErrText(
+			isPreventEn && !isValidName
+				? "全角かな、カナ、漢字で入力してください"
+				: "正しく入力してください。"
+		);
 	} else {
-		if (!val.match(regex)) {
-			if (
-				(preventEn || preventEnDoc) &&
-				(!kanjiKanaRegex.test(nameText) || !kanjiKanaRegex.test(nameTextDoc))
-			) {
-				setErrText("全角かな、カナ、漢字で入力してください");
-			} else {
-				setErrText("正しく入力してください。");
-			}
-		} else {
-			formEle.classList.remove("pageForm__item--valiOn");
-		}
+		formEle.classList.remove("pageForm__item--valiOn");
 	}
 };
 
